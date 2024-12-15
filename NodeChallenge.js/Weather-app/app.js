310fbb38505504fffd147cf5e53f8f1110cd5580
preciousdsmith <precious.d.smith@gmail.com>
2024-12-15 16:56:22 -0500

    updates

--- NodeChallenge.js/Weather-app/app.js
+++ NodeChallenge.js/Weather-app/app.js
@@ -1,29 +1,23 @@
-const request = require('postman-request');
-const geocode = require('../utils/geocode');
-const forecast = require('../utils/forecast');
-//api url
-const url = 'https://api.weatherstack.com/current?access_key=1a103a01b2657215483acc822b54ca41&query=37.8267,-122.4233&units=f'
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const address= process.argv[2];
- //error handling
-console.log(process.argv)
+const address = process.argv[2]
 
-if(!address){
-  console.log('please provide an address')
-}else{
-  geocode(address,(error,{latitude, longitude, location}= {})=>{
-    if(error){
-     return  console.log(error)
-    }
-  
-  forecast(latitude, longitude, (error, forecastData) => {
-    if(error){
-      return console.log(error)
-    }
-  
-    console.log(location)
-    console.log(forecastData)
-  })
-  })
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
 
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
 }
